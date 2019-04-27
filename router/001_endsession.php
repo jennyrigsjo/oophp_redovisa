2ff86fd19299fcfe68830faf7261ecded8aa3dc2
Jenny Rigsjö <jenny.rigsjo@live.com>
2019-04-27 10:50:49 +0100

    Moved endsession-function to separate file to facilitate calling it from other files.

---
+++ router/001_endsession.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Function to terminate the current session (used by the games Guess and Dice 100).
+ * @return void
+ */
+function endSession()
+{
+    // Unset all of the session variables.
+    $_SESSION = [];
+
+    // If it's desired to kill the session, also delete the session cookie.
+    // Note: This will destroy the session, and not just the session data!
+    if (ini_get("session.use_cookies")) {
+        $params = session_get_cookie_params();
+        setcookie(
+            session_name(),
+            '',
+            time() - 42000,
+            $params["path"],
+            $params["domain"],
+            $params["secure"],
+            $params["httponly"]
+        );
+    }
+
+    // Finally, destroy the session.
+    session_destroy();
+    //echo "The session is destroyed.";
+}
